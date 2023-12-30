75c701b0aaede0369b3425a2c1c82d8d7b3b3b23
Elifisci1103@gmail.com <you@example.com>
2023-12-30 21:07:32 +0300

    constants

--- Test_homework.py
+++ Test_homework.py
@@ -4,139 +4,146 @@ from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 from selenium.webdriver.support.wait import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as ec 
-import pytest
-from selenium.webdriver.common.action_chains import ActionChains 
+import openpyxl
+import globalConstants as c
+
+
+
 
 class Test_homework:
   
     def setup_method(self): 
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
-        self.driver.get("https://www.saucedemo.com")
+        self.driver.get(c.URL)
         self.driver.maximize_window() 
 
     def teardown_method(self): 
      self.driver.quit()   
 
+     
+    def getData():
+        excel = openpyxl.load_workbook(c.PATH)
+        sheet = excel["Sayfa1"] #hangi sayfada çalışacağımı gösteriyorum
+        rows = sheet.max_row #kaçıncı satıra kadar veri var?
+        data = []
+        for i in range(2,rows+1):
+            username = sheet.cell(i,1).value
+            password = sheet.cell(i,2).value
+            data.append((username,password))
+
+        return data
+
     def test_invalid_login(self):
         #-Kullanıcı adı ve şifre alanları boş geçildiğinde uyarı mesajı olarak "Epic sadface: Username is required" gösterilmelidir.
     
-        loginButton=WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"login-button")))
+        loginButton=WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,c.LOGIN_BUTTON_ID)))
         loginButton.click()
-        errorMessage=self.driver.find_element(By.XPATH,"//h3[@data-test='error']")
-        assert errorMessage.text== "Epic sadface: Username is required"
+        errorMessage=self.driver.find_element(By.XPATH,c.ERROR_MESSAGE_XPATH)
+        assert errorMessage.text== c.ERROR_MESSAGE_USERNAME_TEXT
 
     def test_invalid_login_username(self):
          #-Sadece şifre alanı boş geçildiğinde uyarı mesajı olarak "Epic sadface: Password is required" gösterilmelidir.
     
-           username=WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//input[@data-test='username']")))
-           username.send_keys("standard_user")
+           username=WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,c.USERNAME_XPATH)))
+           username.send_keys(c.VALID_USERNAME)
           
-           loginButton=self.driver.find_element(By.ID,"login-button")
+           loginButton=self.driver.find_element(By.ID,c.LOGIN_BUTTON_ID)
            loginButton.click()
            
-           errorMessage=WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//h3[@data-test='error']")))
-           assert errorMessage.text== "Epic sadface: Password is required"
+           errorMessage=WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,c.ERROR_MESSAGE_XPATH)))
+           assert errorMessage.text== c.ERROR_MESSAGE_PASSWORD_TEXT
 
     def test_invalid_login_password(self):
 
       #-Kullanıcı adı "locked_out_user" şifre alanı "secret_sauce" gönderildiğinde "Epic sadface: Sorry, this user has been locked out." mesajı gösterilmelidir.
-       username=WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//input[@data-test='username']")))
-       username.send_keys("locked_out_user")
+       username=WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,c.USERNAME_XPATH)))
+       username.send_keys(c.VALID_LOCKED_USERNAME)
 
-       password=self.driver.find_element(By.XPATH,"//input[@data-test='password']")
-       password.send_keys("secret_sauce")
+       password=self.driver.find_element(By.XPATH,c.PASSWORD_XPATH)
+       password.send_keys(c.VALID_PASSWORD)
 
-       loginButton=self.driver.find_element(By.ID,"login-button")
+       loginButton=self.driver.find_element(By.ID,c.LOGIN_BUTTON_ID)
        loginButton.click()
 
-       errorMessage=WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//h3[@data-test='error']")))
-       assert errorMessage.text== "Epic sadface: Sorry, this user has been locked out."
+       errorMessage=WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,c.ERROR_MESSAGE_XPATH)))
+       assert errorMessage.text== c.ERROR_MESSAGE_LOCKED_TEXT
        
     def test_valid_login(self):
 
      #-Kullanıcı adı "standard_user" şifre "secret_sauce" gönderildiğinde kullanıcı "/inventory.html" sayfasına gönderilmelidir. Giriş yapıldıktan sonra kullanıcıya gösterilen ürün sayısı "6" adet olmalıdır.
       
-      username=WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//input[@data-test='username']")))
-      username.send_keys("standard_user")
+      username=WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,c.USERNAME_XPATH)))
+      username.send_keys(c.VALID_USERNAME)
 
-      password=self.driver.find_element(By.XPATH,"//input[@data-test='password']")
-      password.send_keys("secret_sauce")
+      password=self.driver.find_element(By.XPATH,c.PASSWORD_XPATH)
+      password.send_keys(c.VALID_PASSWORD)
 
-      loginButton=self.driver.find_element(By.ID,"login-button")
+      loginButton=self.driver.find_element(By.ID,c.LOGIN_BUTTON_ID)
       loginButton.click()
       sleep(5)
      
-      product_list= self.driver.find_elements(By.XPATH,"//div[@class='inventory_item']")
+      product_list= self.driver.find_elements(By.XPATH,c.PRODUCT_LIST_XPATH)
 
       assert (len(product_list))==6
 
+      
 
-    @pytest.mark.parametrize("username,password",[("standard_user","ab"),("problem_user","ba"),("standard_user","secret_sauce")])
-    def test_parametrized_login(self,username,password):
-       #Sauce Demo sayfasına parametreler ile 2 başarısız 1 başarılı girişi test ediniz.
-        username = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
-        username.send_keys(username)
-        password= WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
-        password.send_keys(password)
-        loginButton = self.driver.find_element(By.ID,"login-button")
-        loginButton.click()
-    
 
     def test_order_product(self):
         # Sauce Demo sayfasına giriş yaparak ürünleri price(low to high) olarak sıralayın ve 3. ürünün fiyatının 15.99$ olduğunu test ediniz.
-        username = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
-        username.send_keys("standard_user")
-        password= WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
-        password.send_keys("secret_sauce")
-        loginButton = self.driver.find_element(By.ID,"login-button")
+        username = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,c.USERNAME_XPATH)))
+        username.send_keys(c.VALID_USERNAME)
+        password= WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,c.PASSWORD_XPATH)))
+        password.send_keys(c.VALID_PASSWORD)
+        loginButton = self.driver.find_element(By.ID,c.LOGIN_BUTTON_ID)
         loginButton.click()
-        sortButton= WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CLASS_NAME,"product_sort_container")))
+        sortButton= WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CLASS_NAME,c.SORT_BUTTON_CLASS)))
         sortButton.click()
         sleep(3)
-        lowToHigh=self.driver.find_element(By.XPATH,"//option[text()='Price (low to high)']")
+        lowToHigh=self.driver.find_element(By.XPATH,c.LOW_TO_HIGH_XPATH)
         lowToHigh.click()
-        productPrice=self.driver.find_element(By.XPATH,"(//div[@class='inventory_item_price'])[3]")
-        assert productPrice.text =="$15.99" 
+        productPrice=self.driver.find_element(By.XPATH,c.PRODUCT_PRICE_XPATH)
+        assert productPrice.text == c.PRODUCT_PRICE 
         
         
 
     def test_buy_product(self):
         #Sauce Demo sayfasına giriş yaparak  herhangi bir ürünü sepete atıp gerekli işlemleri yaparak ""Thank you for your order!" yazısını görmeyi test ediniz.
 
-        username = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
-        username.send_keys("standard_user")
-        password= WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
-        password.send_keys("secret_sauce")
+        username = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,c.USERNAME_ID)))
+        username.send_keys(c.VALID_USERNAME)
+        password= WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,c.PASSWORD_ID)))
+        password.send_keys(c.VALID_PASSWORD)
         sleep(3)
-        loginButton = self.driver.find_element(By.ID,"login-button")
+        loginButton = self.driver.find_element(By.ID,c.LOGIN_BUTTON_ID)
         loginButton.click()
-        firstProduct = WebDriverWait(self.driver,10).until(ec.visibility_of_element_located((By.ID,"item_4_title_link")))
+        firstProduct = WebDriverWait(self.driver,10).until(ec.visibility_of_element_located((By.ID,c.FIRST_PRODUCT_ID)))
         firstProduct.click()
         sleep(2)
-        addToCart=self.driver.find_element(By.ID,"add-to-cart-sauce-labs-backpack")
+        addToCart=self.driver.find_element(By.ID,c.ADD_TO_CART_BUTTON_ID)
         addToCart.click()
         sleep(2)
-        shoppingCart=self.driver.find_element(By.XPATH,"//div[@class='shopping_cart_container']")
+        shoppingCart=self.driver.find_element(By.XPATH,c.SHOPPING_CART_XPATH)
         shoppingCart.click()
         sleep(2)
-        checkoutButton=self.driver.find_element(By.ID,"checkout")
+        checkoutButton=self.driver.find_element(By.ID,c.CHECKOUT_BUTTON_ID)
         checkoutButton.click()
         sleep(3)
-        firstNameText=self.driver.find_element(By.ID,"first-name")
-        lastNameText=self.driver.find_element(By.ID,"last-name")
-        postalCodeText=self.driver.find_element(By.ID,"postal-code")
+        firstNameText=self.driver.find_element(By.ID,c.FIRSTNAME_TEXT_ID)
+        lastNameText=self.driver.find_element(By.ID,c.LASTNAME_TEXT_ID)
+        postalCodeText=self.driver.find_element(By.ID,c.POSTALCODE_TEXT_ID)
         
-        firstNameText.send_keys("elif")
-        lastNameText.send_keys("isci")
-        postalCodeText.send_keys("34000")
+        firstNameText.send_keys(c.FIRSTNAME_TEXT)
+        lastNameText.send_keys(c.LASTNAME_TEXT)
+        postalCodeText.send_keys(c.POSTALCODE_TEXT)
 
-        continueButton=WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"continue")))
+        continueButton=WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,c.CONTINUE_BUTTON_ID)))
         continueButton.click()
 
-        finishButton=self.driver.find_element(By.ID,"finish")
+        finishButton=self.driver.find_element(By.ID,c.FINISH_BUTTON_ID)
         finishButton.click()
-        sleep(30)
-        orderMessage=self.driver.find_element(By.CLASS_NAME,"complete-header")
-        assert orderMessage.text == "Thank you for your order!"
+        sleep(3)
+        orderMessage=self.driver.find_element(By.CLASS_NAME,c.ORDER_MESSAGE_CLASS)
+        assert orderMessage.text == c.ORDER_MESSAGE
         
         
